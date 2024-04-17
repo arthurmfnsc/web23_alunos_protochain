9f86e42c20c7e5872f0af2e140cee4feea576389
Arthur Fonseca <144041324+arthurmfnsc@users.noreply.github.com>
2024-04-17 19:08:31 -0300

    ✨ Implement block chain reference with previous block (#5)

--- __tests__/blockchain.test.ts
+++ __tests__/blockchain.test.ts
@@ -1,9 +1,33 @@
+import Block from "../src/lib/block";
 import Blockchain from "../src/lib/blockchain";
 
 describe("Blockchain tests", () => {
     
     test("Should has genesis block", () => {
         const blockchain = new Blockchain();
-        expect(blockchain.blocks.length).toEqual(1);
+        expect(blockchain.getBlocks().length).toEqual(1);
+    })
+
+    test("Should be valid (genesis)", () => {
+        const blockchain = new Blockchain();
+        expect(blockchain.isValid()).toBeTruthy();
+    })
+
+    test("Should be valid (two blocks)", () => {
+        const blockchain = new Blockchain();
+        blockchain.addBlock(new Block(1, blockchain.getBlocks()[0].getHash(), "Block 2"));
+        expect(blockchain.isValid()).toBeTruthy();
+    })
+
+    test("Should add block", () => {
+        const blockchain = new Blockchain();
+        const result = blockchain.addBlock(new Block(1, blockchain.getBlocks()[0].getHash(), "Block 2"));
+        expect(result).toBeTruthy();
+    })
+
+    test("Should not add block", () => {
+        const blockchain = new Blockchain();
+        const result = blockchain.addBlock(new Block(-1, blockchain.getBlocks()[0].getHash(), "Block 2"));
+        expect(result).toBeFalsy();
     })
 })
