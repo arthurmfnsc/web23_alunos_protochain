1e2985bcc3d252e101d5d06c957c815f7587dcd3
Arthur Fonseca <144041324+arthurmfnsc@users.noreply.github.com>
2024-04-16 22:59:22 -0300

    🎉 Project setup (#1)

---
+++ __tests__/blockchain.test.ts
@@ -0,0 +1,9 @@
+import Blockchain from "../src/lib/blockchain";
+
+describe("Blockchain tests", () => {
+    
+    test("Should has genesis block", () => {
+        const blockchain = new Blockchain();
+        expect(blockchain.blocks.length).toEqual(1);
+    })
+})
